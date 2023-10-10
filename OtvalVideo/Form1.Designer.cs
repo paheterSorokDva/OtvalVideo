8bc9e73bbd0cb53d336efc7181ef3e7650cf992f
Paheter <111425098+paheterSorokDva@users.noreply.github.com>
2023-10-10 20:03:38 +0700

    W
    Up

---
+++ OtvalVideo/Form1.Designer.cs
@@ -0,0 +1,68 @@
+﻿namespace OtvalVideo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(848, 480);
+            this.ControlBox = false;
+            this.Enabled = false;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.Text = "Form1";
+            this.TopMost = true;
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        public System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ OtvalVideo/Form1.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Configuration;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Timers;
+using System.Windows.Forms;
+using System.Windows.Forms.Design;
+
+namespace OtvalVideo
+{
+    public partial class Form1 : Form
+    {
+
+        // Для хранения скрина
+        public static Bitmap BM = new Bitmap(Screen.PrimaryScreen.Bounds.Width, Screen.PrimaryScreen.Bounds.Height);
+        Graphics GH = Graphics.FromImage(BM as Image);
+
+        Random rnd = new Random();
+
+        static System.Windows.Forms.Timer aTimer = new System.Windows.Forms.Timer();
+
+        // bool _closeoff = true;
+        bool _closeoff = false; // изменить на тру в конце
+        public Form1()
+        {
+            InitializeComponent();
+            ShowInTaskbar = false;
+            var fullscreenSize = new Size(Screen.PrimaryScreen.Bounds.Width, Screen.PrimaryScreen.Bounds.Height);
+            Size = fullscreenSize;
+
+            //aTimer.Enabled = true;
+
+            aTimer.Tick += new EventHandler(timer1_Tick_1);
+            aTimer.Interval = 33;
+            
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // Скрин
+            //Graphics GH = Graphics.FromImage(BM as Image);
+            GH.CopyFromScreen(0, 0, 0, 0, BM.Size);
+
+            /// отключить бордюр
+            FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+
+            /// Делаем окно на весь экран
+            WindowState = FormWindowState.Maximized;
+
+            /// Поверх всех
+            TopMost = true;
+
+            /// отключаем курсор
+            Cursor.Hide();
+
+            /// меняем цвет формы
+            BackColor = Color.Black;
+
+
+            
+            BackgroundImage = BM;
+
+            
+
+
+
+
+
+
+        }
+
+
+       
+
+
+
+
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+            
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            e.Cancel = _closeoff; // блокировка закрытия
+            
+
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.SuppressKeyPress = e.Alt || e.KeyCode == Keys.F4) // Блокируем  ALT+F4
+            {
+                _closeoff = true;
+            }
+
+            if (e.SuppressKeyPress = e.Control && e.KeyCode == Keys.Z) // Присваиваем новый ALT+F4
+            {
+                _closeoff = false;
+                Application.Exit();
+            }
+
+            
+
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            //Random rnd = new Random();
+            //Graphics GH = Graphics.FromImage(BM as Image);
+           GH.CopyFromScreen(rnd.Next(-5, 11), 0, rnd.Next(-5, 11), 0, BM.Size);
+
+
+        }
+
+
+
+
+        private static void OnTimedEvent(Object source, ElapsedEventArgs e)
+        {
+            //Random rnd = new Random();
+           // Graphics GH = Graphics.FromImage(BM as Image);
+            //GH.CopyFromScreen(rnd.Next(-5, 11), 0, rnd.Next(-5, 11), 0, BM.Size);
+
+            
+
+        }
+
+        private void timer1_Tick_1(object sender, EventArgs e)
+        {
+            
+            
+        }
+    }
+}
